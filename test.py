5476bfc2ed3c1e3dbcc4b9f9d8d028de92f16e18
blockboy05 <92528950+blockboy05@users.noreply.github.com>
2022-09-10 10:53:30 +0100

    Test that IDE is working properly

---
+++ test.py
@@ -0,0 +1,9 @@
+import sys #imports the sys libary
+import random #imports the random libary
+import csv #imports the csv libary
+import tkinter #imports the tkinter libary
+
+print ("Hello World")
+
+hello=input("...")
+sys.exit
